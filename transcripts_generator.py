8947fa9a75967221b01421bc54117f0f2aa075d2
Anh Quan Truong <quan@Anhs-MacBook-Air.local>
2024-03-14 05:44:57 -0400

    added generator for random transcripts

---
+++ transcripts_generator.py
@@ -0,0 +1,38 @@
+from tools_libs import *
+
+def transcripts_generator(csv_file, num_students=50):
+    # read the csv file
+    df = pd.read_csv(csv_file)
+
+    # extract the available courses
+    course_codes = df['Course code'].tolist()
+    course_nums  = df['Course number'].tolist()
+
+    # generate transcript for each student
+    transcripts = []
+    for student in range(num_students):
+        # select random courses (5 - 10 courses per student)
+        num_courses = random.randint(5, 10)
+        courses = random.sample(list(zip(course_codes, course_nums)), num_courses)
+
+        # generate grade(s) for each class
+        grades = {}
+        for course in courses:
+            grade = random.randint(50, 100)
+            grades[course] = [grade]
+
+            # check and see if the student needs to retake the class
+            if grade < 60:
+                retake_grade = random.randint(50, 100)
+                grades[course].append(retake_grade)
+
+        transcripts.append({'courses': courses, 'grades': grades})
+
+    # create a dataframe from the transcripts
+    transcripts_df = pd.DataFrame(transcripts)
+
+    # convert the df into a csv file
+    transcripts_df.to_csv('data/students/transcripts.csv', index=False)
+
+# randomly generate 50 transcripts
+transcripts_generator('data/courses/processed_course_info_2024.csv', 50)
