f651e85cad1c0b44d0b27e482458a7f1e78ac18d
Anh Quan Truong <quan@Anhs-MacBook-Air.local>
2024-03-14 05:44:23 -0400

    made a seperate file for necessary modules and constants

---
+++ tools_libs.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import random
+import ast
+
+from rdflib import Graph, Namespace, URIRef, Literal, XSD
+from rdflib.namespace import RDF, RDFS, FOAF
+
+# define rdf namespaces
+ex = Namespace("http://example.org/")
+dbp = Namespace("http://dbpedia.org/resource/")
+
+# constants
+UNIVERSITY = dbp.Concordia_University
