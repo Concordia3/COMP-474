c1c79f8c304801778c69469862df5fd2d4297c5b
Anh Quan Truong <quanat1x4@gmail.com>
2024-03-15 16:17:01 -0400

    added random seed

--- tools_libs.py
+++ tools_libs.py
@@ -3,6 +3,9 @@ import random
 import ast
 import os
 
+# Set the seed value
+random.seed(0)
+
 from rdflib import Graph, Namespace, URIRef, Literal, XSD
 from rdflib.namespace import RDF, RDFS, FOAF
 
