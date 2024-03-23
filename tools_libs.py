91825a16c2bbf0ad079d13fa8f07015b6b040695
Anh Quan Truong <quanat1x4@gmail.com>
2024-03-22 20:44:17 -0400

    removed random and seed

--- tools_libs.py
+++ tools_libs.py
@@ -3,9 +3,6 @@ import random
 import ast
 import os
 
-# Set the seed value
-random.seed(0)
-
 from rdflib import Graph, Namespace, URIRef, Literal, XSD
 from rdflib.namespace import RDF, RDFS, FOAF
 
