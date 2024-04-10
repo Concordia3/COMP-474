550dd38c13bd4337eabc3440dff69889f2aeb53e
Anh Quan Truong <quanat1x4@gmail.com>
2024-04-10 18:39:56 -0400

    added more global variables

--- tools_libs.py
+++ tools_libs.py
@@ -14,6 +14,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 from rdflib import Graph, Namespace, URIRef, Literal, XSD
 from rdflib.namespace import RDF, RDFS, FOAF
 
+# initialize language model
+nlp = spacy.load("en_core_web_lg")
+
+# set of cs concepts that will be used to filter out the topics
+with open('topics_excavator/cs_concepts.json') as file:
+    data = json.load(file)
+cs_concepts = set(data['cs_concepts'])
+
 # define rdf namespaces
 ex = Namespace("http://example.org/")
 dbp = Namespace("http://dbpedia.org/resource/")
