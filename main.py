81a07e513a6f2052840ae0ef667eb9e38dc5b340
Anh Quan Truong <quanat1x4@gmail.com>
2024-04-10 18:39:18 -0400

    updated main script

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ subprocess.run('pip install -r requirements.txt', shell=True,
 ############################################################################################################
 from tools_libs import *
 from courses_graph_related_files.courses_rdf import courses_rdf
-from profiles_graph_related_files.transcripts_generator import transcripts_generator
 from courses_graph_related_files.comp_courses_rdf import comp_courses_rdf
+from courses_graph_related_files.topics_rdf import topics_rdf
+from profiles_graph_related_files.transcripts_generator import transcripts_generator
 from profiles_graph_related_files.profiles_generator import profiles_generator
 from profiles_graph_related_files.profiles_rdf import profiles_rdf
 from topics_excavator.topics_excavator import topics_excavator
@@ -36,28 +37,26 @@ subprocess.run('python -m spacy download en_core_web_lg',
 # make dfs from the csv files
 df_info = pd.read_csv('data/courses/raw_course_info_2024.csv')
 df_web  = pd.read_csv('data/courses/raw_course_websites_2024.csv')
-df_desc = pd.read_csv('data/courses/raw_course_desc_2024.csv')
 
 # rename column in df_info to match the column name in df_web
 df_info.rename(columns={'Subject': 'Course code', 'Catalog': 'Course number'}, inplace=True)
 
 # merge the dataframes
-merged_df_1 = pd.merge(df_info, df_web, on=['Course code', 'Course number'], how='inner')
-merged_df_2 = pd.merge(merged_df_1, df_desc, on=['Course ID'], how='inner')
+merged_df = pd.merge(df_info, df_web, on=['Course code', 'Course number'], how='inner')
 
 # replace NaN values with 'No website provided'
-merged_df_2['Website'] = merged_df_2['Website'].fillna('No website provided')
-merged_df_2['Website'] = merged_df_2['Website'].replace(" ", 'No website provided')
+merged_df['Website'] = merged_df['Website'].fillna('No website provided')
+merged_df['Website'] = merged_df['Website'].replace(" ", 'No website provided')
 
 # save the merged dataframe
-merged_df_2.to_csv('data/courses/processed_course_info_2024.csv', index=False)
+merged_df.to_csv('data/courses/processed_course_info_2024.csv', index=False)
 
 ############################################################################################################
 # make an rdf graph with processed course csv file                                                          #
 ############################################################################################################
 courses_graph = Graph()
 
-course_graph = courses_rdf(merged_df_2)
+course_graph = courses_rdf(merged_df)
 
 # save the graph
 course_graph.serialize(destination="graphs/courses.ttl", format="turtle") 
@@ -65,11 +64,20 @@ course_graph.serialize(destination="graphs/courses.ttl", format="turtle")
 ############################################################################################################
 # make an rdf graph with the info provided on COMP 472 and COMP 474                                        #
 ############################################################################################################
-# make the graph
-comp_courses_graph = comp_courses_rdf('data/courses','graphs/courses.ttl')
+# make the comp courses graph
+comp_courses_graph = comp_courses_rdf('data/courses','graphs/courses.ttl', nlp, cs_concepts)
 
 # save the comp courses graph
 comp_courses_graph.serialize("graphs/comp_courses.ttl", format="turtle")
+
+############################################################################################################
+# make an rdf graph for topics found in comp courses                                                       #
+############################################################################################################
+# make the topics graph
+topics_graph = topics_rdf("graphs/comp_courses.ttl")
+
+# save the topics graph
+topics_graph.serialize("graphs/topics.ttl", format="turtle")
 
 ############################################################################################################
 # randomly generate transcripts based on the courses found in the processed csv above                      #
@@ -92,7 +100,7 @@ profiles_df.to_csv('data/students/profiles.csv', index=False)
 ############################################################################################################
 profiles_graph = Graph()
 
-profiles_graph = profiles_rdf(profiles_df, 'graphs/courses.ttl')
+profiles_graph = profiles_rdf(profiles_df, 'graphs/courses.ttl', 'graphs/comp_courses.ttl')
 
 # save the graph
 profiles_graph.serialize(destination="graphs/profiles.ttl", format="turtle")
@@ -100,75 +108,30 @@ profiles_graph.serialize(destination="graphs/profiles.ttl", format="turtle")
 ############################################################################################################
 # merge graphs                                                                                             #
 ############################################################################################################
-# merge courses and comp courses graphs
+# courses graph
 courses_graph = Graph()
 courses_graph.parse("graphs/courses.ttl", format='turtle')
 
+# comp courses graph
 comp_courses_graph = Graph()
 comp_courses_graph.parse("graphs/comp_courses.ttl", format='turtle')
 
-courses_graph += comp_courses_graph
+# topics graph
+topics_graph = Graph()
+topics_graph.parse("graphs/topics.ttl", format='turtle')
 
-# merge the courses and profiles graphs
+# profiles graph
 profiles_graph = Graph()
 profiles_graph.parse("graphs/profiles.ttl", format='turtle')
 
+# merge the courses and topics graphs
+comp_courses_graph += topics_graph
+
+# merge the courses and comp courses graphs
+courses_graph += comp_courses_graph
+
+# merge the courses and profiles graphs
 university_graph = courses_graph + profiles_graph
 
 # save the final graph
 university_graph.serialize(destination="graphs/university.ttl", format="turtle")
-
-############################################################################################################
-# read pdf files and extract text                                                                          #
-############################################################################################################
-# initialize language model
-nlp = spacy.load("en_core_web_lg")
-
-# extract all entities (topics) from comp472 and comp474 pdf files
-data_path = 'data/courses/'
-
-# write the results to a csv file
-csv_file = open('topics_excavator/detected_topics.csv', 'w', newline='')
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(["Document Name", "Document Path", "Topics", "URIs"])
-
-# set of cs concepts that will be used to filter out the topics
-with open('topics_excavator/cs_concepts.json') as file:
-    data = json.load(file)
-cs_concepts = set(data['cs_concepts'])
-
-# data folder
-for folder in os.listdir(data_path):
-    folder_path = os.path.join(data_path, folder)
-
-    # if folder is indeed a folder
-    if os.path.isdir(folder_path):
-
-        # iterate over the items in the folder
-        for item in os.listdir(folder_path):
-            item_path = os.path.join(folder_path, item)
-
-            # syllabus
-            if item.endswith('.pdf'):
-                topics = topics_excavator(item_path, nlp, cs_concepts)
-
-                for topic in topics:
-                    topic_name = topic[0]
-                    topic_uri = topic[1]
-                    csv_writer.writerow([item, item_path, topic_name, topic_uri])
-            
-            # if folders (lectures, worksheets, etc.)
-            elif os.path.isdir(item_path):
-
-                # iterate over the files in the folder
-                for file in os.listdir(item_path):
-                    file_path = os.path.join(item_path, file)
-
-                    if file.endswith('.pdf'):
-                        topics = topics_excavator(file_path, nlp, cs_concepts)
-
-                        for topic in topics:
-                            topic_name = topic[0]
-                            topic_uri = topic[1]
-                            csv_writer.writerow([file, file_path, topic_name, topic_uri])
-csv_file.close()
