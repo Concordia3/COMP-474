18f3c2b9d1144481a2d5ca4670e87a16ecc87ded
Anh Quan Truong <quanat1x4@gmail.com>
2024-04-10 18:38:24 -0400

    added skills to student if they have passed either comp474 or comp472

--- profiles_graph_related_files/profiles_rdf.py
+++ profiles_graph_related_files/profiles_rdf.py
@@ -1,14 +1,29 @@
 from tools_libs import *
 
-def profiles_rdf(df, courses_graph_path):
+def profiles_rdf(df, courses_graph_path, comp_courses_graph_path):
     # load the courses graph
     courses_graph = Graph()
     courses_graph.parse(courses_graph_path, format='turtle')
+
+    # load the comp courses graph
+    comp_courses_graph = Graph()
+    comp_courses_graph.parse(comp_courses_graph_path, format='turtle')
 
     # iterate over the courses graph and extract the classes
     course_class    = None
     for s, p, o in courses_graph.triples((None, RDF.type, RDFS.Class)):
         if   s == ex.Course: course_class = s
+
+    # iterate over the comp courses graph and extract the skills
+    comp_472_skills = []
+    comp_474_skills = []
+    for s, p, o in comp_courses_graph.triples((None, RDF.type, ex.Syllabus)):
+        if s == ex['COMP' + '/' + '472' + '/' + 'syllabus.pdf']:
+            for s1, p1, o1 in comp_courses_graph.triples((s, ex.contentTopic, None)):
+                comp_472_skills.append(o1)
+        elif s == ex['COMP' + '/' + '474' + '/' + 'syllabus.pdf']:
+            for s1, p1, o1 in comp_courses_graph.triples((s, ex.contentTopic, None)):
+                comp_474_skills.append(o1)
 
     # create an empty graph
     g = Graph()
@@ -18,6 +33,7 @@ def profiles_rdf(df, courses_graph_path):
     g.add((ex.hasId, RDF.type, RDF.Property))          # id property
     g.add((ex.isEnrolled, RDF.type, RDF.Property))     # enrolled property
     g.add((ex.studiesAt, RDF.type, RDF.Property))      # studies at property
+    g.add((ex.hasSkill, RDF.type, RDF.Property))       # skills property
 
     g.add((ex.Grade, RDF.type, RDFS.Class))            # grade class
     g.add((ex.hasGradeVal, RDF.type, RDF.Property))    # value property
@@ -74,8 +90,16 @@ def profiles_rdf(df, courses_graph_path):
 
                     grade_uri = URIRef(course_uri + '/' + str(id) + '/' + str(counter))         # create a URI for the grade
 
-                    if grade < 60: g.add((grade_uri, ex.gradeStatus, Literal('F')))             # grade < 60 is a fail
-                    else:          g.add((grade_uri, ex.gradeStatus, Literal('P')))             # otherwise, it's a pass
+                    if grade < 60: 
+                        g.add((grade_uri, ex.gradeStatus, Literal('F')))                        # grade < 60 is a fail
+                    else: 
+                        g.add((grade_uri, ex.gradeStatus, Literal('P')))                        # otherwise, it's a pass
+                        if course_code == 'COMP' and course_number == '472':                    # if the course is COMP 472
+                            for skill in comp_472_skills:
+                                g.add((student_uri, ex.hasSkill, skill))                        # add the skills to the student
+                        elif course_code == 'COMP' and course_number == '474':                  # if the course is COMP 474
+                            for skill in comp_474_skills:
+                                g.add((student_uri, ex.hasSkill, skill))                        # add the skills to the student
 
                     g.add((grade_uri, RDF.type, ex.Grade))                                      # specify that the URI is a grade
                     g.add((grade_uri, ex.hasGradeVal, Literal(grade, datatype=XSD.integer)))    # add the grade value
