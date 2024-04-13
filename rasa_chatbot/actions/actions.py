b0fe92c72cfdf6df5d8e1d331dfe99319dc7c550
eehamkhan <khan.meeham@gmail.com>
2024-04-13 13:48:20 -0400

    Added intents and entities within domain. Created action for second query from Project Part 1.

--- rasa_chatbot/actions/actions.py
+++ rasa_chatbot/actions/actions.py
@@ -3,6 +3,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action
 from SPARQLWrapper import SPARQLWrapper, JSON
 
+
 class ActionListCourses(Action):
     def name(self):
         return "action_list_courses"
@@ -12,7 +13,7 @@ class ActionListCourses(Action):
         if university is None:
             university = ''
 
-        sparql = SPARQLWrapper("http://localhost:3030/ds/query")  # Corrected URL
+        sparql = SPARQLWrapper("http://localhost:3030/concordia/query")
         sparql.setQuery(f"""
             PREFIX ns1: <http://example.org/>
             PREFIX dbp: <http://dbpedia.org/resource/>
@@ -21,7 +22,6 @@ class ActionListCourses(Action):
             WHERE {{
                 ?course ns1:courseAt dbp:Concordia_University
             }}
-            LIMIT 10
         """)
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
@@ -29,6 +29,41 @@ class ActionListCourses(Action):
         message = "Courses at Concordia:\n"
         for result in results["results"]["bindings"]:
             message += f'- {result["course"]["value"]}\n'
+
+        dispatcher.utter_message(text=message)
+
+        return []
+
+
+class DiscussedTopic(Action):
+    def name(self):
+        return "action_discussed_topic"
+
+    def run(self, dispatcher, tracker, domain):
+        topic = tracker.get_slot('topic')
+        if topic is None:
+            topic = ''
+
+        sparql = SPARQLWrapper("http://localhost:3030/concordia/query")
+        sparql.setQuery(f"""
+            PREFIX ns1: <http://example.org/>
+        
+            SELECT ?courseTitle
+            WHERE {{
+            ?course a ns1:Course;
+            ns1:hasTitle ?courseTitle;
+            ns1:hasDescription ?description;
+            FILTER
+            regex(?description, "{tracker.slots['topic']}", "i").
+            }}
+
+    """)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+
+        message = "The courses are:\n"
+        for result in results["results"]["bindings"]:
+            message += f'- {result["courseTitle"]["value"]}\n'
 
         dispatcher.utter_message(text=message)
 
